06bfe92a7cfd3f9c4c113d0c2f1c424aaf89b48e
Raphael von der Grün <raphinesse@gmail.com>
2025-08-21 21:31:47 +0000

    Remove parallelism support from BucketPointers and simplify pointer management

--- include/ips4o/bucket_pointers.hpp
+++ include/ips4o/bucket_pointers.hpp
@@ -52,121 +52,29 @@ template <class Cfg>
 class Sorter<Cfg>::BucketPointers {
     using diff_t = typename Cfg::difference_type;
 
-#if UINTPTR_MAX == UINT32_MAX || defined(__SIZEOF_INT128__)
-
-#if UINTPTR_MAX == UINT32_MAX
-
-    using atomic_type = std::uint64_t;
-
-#elif defined(__SIZEOF_INT128__)
-
-    using atomic_type = __uint128_t;
-
-#endif  // defined( __SIZEOF_INT128__)
-
-    // alignas(std::hardware_destructive_interference_size)
-    class Uint128 {
-     public:
-        inline void set(diff_t l, diff_t m) {
-            single_.m_ = m;
-            single_.l_ = l;
-        }
-
-        inline diff_t getLeastSignificant() const { return single_.l_; }
-
-        inline std::pair<diff_t, diff_t> fetchSubMostSignificant(diff_t m) {
-            const auto tmp = single_.m_;
-            single_.m_ -= m;
-            return {single_.l_, tmp};
-        }
-
-        inline std::pair<diff_t, diff_t> fetchAddLeastSignificant(diff_t l) {
-            const auto tmp = single_.l_;
-            single_.l_ += l;
-            return {tmp, single_.m_};
-        }
-
-     private:
-        static constexpr const int kShift = sizeof(atomic_type) * CHAR_BIT / 2;
-        static constexpr const atomic_type kMask =
-                (static_cast<atomic_type>(1) << kShift) - 1;
-
-        struct Pointers {
-            diff_t l_, m_;
-        };
-        union {
-            atomic_type all_;
-            Pointers single_;
-        };
-    };
-
-#else
-
-    class Uint128 {
-     public:
-        inline void set(diff_t l, diff_t m) {
-            m_ = m;
-            l_ = l;
-        }
-
-        inline diff_t getLeastSignificant() const { return l_; }
-
-        template <bool kAtomic>
-        inline std::pair<diff_t, diff_t> fetchSubMostSignificant(diff_t m) {
-            if (kAtomic) {
-                std::lock_guard<std::mutex> lock(mtx_);
-                std::pair<diff_t, diff_t> p{l_, m_};
-                m_ -= m;
-                return p;
-            } else {
-                const auto tmp = m_;
-                m_ -= m;
-                return {l_, tmp};
-            }
-        }
-
-        template <bool kAtomic>
-        inline std::pair<diff_t, diff_t> fetchAddLeastSignificant(diff_t l) {
-            if (kAtomic) {
-                std::lock_guard<std::mutex> lock(mtx_);
-                std::pair<diff_t, diff_t> p{l_, m_};
-                l_ += l;
-                return p;
-            } else {
-                const auto tmp = l_;
-                l_ += l;
-                return {tmp, m_};
-            }
-        }
-
-     private:
-        diff_t m_, l_;
-        std::mutex mtx_;
-    };
-
-#endif
-
  public:
     /**
      * Sets write/read pointers.
      */
     void set(diff_t w, diff_t r) {
-        ptr_.set(w, r);
-        num_reading_.store(0, std::memory_order_relaxed);
+        read_ = r;
+        write_ = w;
     }
 
     /**
      * Gets the write pointer.
      */
     diff_t getWrite() const {
-        return ptr_.getLeastSignificant();
+        return write_;
     }
 
     /**
      * Gets write/read pointers and increases the write pointer.
      */
     std::pair<diff_t, diff_t> incWrite() {
-        return ptr_.fetchAddLeastSignificant(Cfg::kBlockSize);
+        const auto tmp = write_;
+        write_ += Cfg::kBlockSize;
+        return {tmp, read_};
     }
 
     /**
@@ -174,28 +82,13 @@ class Sorter<Cfg>::BucketPointers {
      * counter.
      */
     std::pair<diff_t, diff_t> decRead() {
-        return ptr_.fetchSubMostSignificant(Cfg::kBlockSize);
-    }
-
-    /**
-     * Decreases the read counter.
-     */
-    void stopRead() {
-        // Synchronizes with threads wanting to write to this bucket
-        num_reading_.fetch_sub(1, std::memory_order_release);
-    }
-
-    /**
-     * Returns true if any thread is currently reading from here.
-     */
-    bool isReading() {
-        // Synchronizes with threads currently reading from this bucket
-        return num_reading_.load(std::memory_order_acquire) != 0;
+        const auto tmp = read_;
+        read_ -= Cfg::kBlockSize;
+        return {write_, tmp};
     }
 
  private:
-    Uint128 ptr_;
-    std::atomic_int num_reading_;
+    diff_t write_, read_;
 };
 
 }  // namespace detail
